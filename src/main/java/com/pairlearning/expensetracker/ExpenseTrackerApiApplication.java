2dbab2e0d8c8400fdd21565dc41e2e2427b2df7e
rosca_ioana <rosca.ioana11@gmail.com>
2021-01-04 20:13:53 +0200

    Adding new categories

--- src/main/java/com/pairlearning/expensetracker/ExpenseTrackerApiApplication.java
+++ src/main/java/com/pairlearning/expensetracker/ExpenseTrackerApiApplication.java
@@ -1,13 +1,25 @@
 package com.pairlearning.expensetracker;
 
+import com.pairlearning.expensetracker.filters.AuthFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ExpenseTrackerApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExpenseTrackerApiApplication.class, args);
+	}
+
+	@Bean
+	public FilterRegistrationBean<AuthFilter> filterRegistrationBean(){  //to protect the resources
+		FilterRegistrationBean<AuthFilter> registrationBean = new FilterRegistrationBean<>();
+		AuthFilter authFilter = new AuthFilter();
+		registrationBean.setFilter(authFilter);
+		registrationBean.addUrlPatterns("/api/categories/*");
+		return registrationBean;
 	}
 
 }
---
+++ src/main/java/com/pairlearning/expensetracker/controller/CategoryController.java
@@ -0,0 +1,20 @@
+package com.pairlearning.expensetracker.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+
+    @GetMapping("")
+    public String getAllCategories(HttpServletRequest request){
+        int userId = (Integer) request.getAttribute("userId");
+        return "Authenticated! UserId: " + userId;
+    }
+
+}
